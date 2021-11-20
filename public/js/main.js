b1f6e3ca452d1652e606a437c05bd559d1199dfa
hanuman shaik <hanumanbabu.shaik@devrabbit.com>
2021-11-20 20:35:31 +0530

    fixed cors issue and added image

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -11,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     // console.log('server', search.value);
-    let searchLocation = `http://api.weatherstack.com/current?access_key=0e61d2b26ddd9dfa30eba0e76f3944cf&query=${encodeURIComponent(search.value)}`;
+    let searchLocation = `https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=0e61d2b26ddd9dfa30eba0e76f3944cf&query=${encodeURIComponent(search.value)}`;
     fetch(searchLocation).then((res) => {
         return res.json();
     }).then((data) => {
